59ab0cc7409a077a61df338b0d02b59be671188d
Willem Bakker <wimb64@icloud.com>
2024-04-16 14:21:51 +0200

    refactor: remove my-pets view, move functionality to controller

--- resources/views/pets/index.blade.php
+++ resources/views/pets/index.blade.php
@@ -1,9 +1,12 @@
 <x-app-layout>
-    <div class="mx-auto p-4 sm:p-6 lg:p-8">
+    <div class="max-w-7xl mx-auto p-4 sm:p-6 lg:p-8">
         <div class="mt-6 bg-white shadow-sm rounded-lg divide-y">
             @foreach ($pets as $pet)
                 @include('pets.show', ['pet' => $pet, 'showRespondButton' => true])
             @endforeach
         </div>
+        @if(Route::is('pets.my-pets'))
+            @include('pets.partials.create-pet-form')
+        @endif
     </div>
 </x-app-layout>
--- resources/views/pets/show.blade.php
+++ resources/views/pets/show.blade.php
@@ -1,3 +1,5 @@
+{{-- shows one pet and its advert --}}
+
 <div class="py-12">
     <div class="mx-auto sm:px-6 lg:px-8 grid md:grid-cols-2 gap-4">
         <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
