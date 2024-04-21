b1727db4254c85319cb26e9f2fd90e3a56010f5f
Willem Bakker <wimb64@icloud.com>
2024-04-21 19:21:02 +0200

    feature: add some seeders and factories

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,11 +12,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $this->call(UserSeeder::class);
+        $this->call(PetSeeder::class);
+        $this->call(PetsitterAdvertSeeder::class);
+//        $this->call(AdvertResponseSeeder::class);
+//        $this->call(PetsitterAdvertResponseSeeder::class);
     }
 }
