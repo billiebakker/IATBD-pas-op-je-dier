0fbe7b353b1175c4f7f8010563f26f2c1772b808
Willem Bakker <wimb64@icloud.com>
2024-04-21 17:55:52 +0200

    feature: add filter pets

--- resources/views/pets/index.blade.php
+++ resources/views/pets/index.blade.php
@@ -23,7 +23,30 @@
             </div>
         @endif
 
-        @includeWhen(Route::is('pets.my-pets'), 'pets.partials.create-pet-form')
+
+            @includeWhen(Route::is('pets.my-pets'), 'pets.partials.create-pet-form')
+
+            <div class="py-2">
+                <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                    <form action="{{ route('pets.index') }}" method="GET">
+                        <div class="p-4 bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                            <label for="type">Filter by Animal Type</label>
+                            <select name="type" id="type" class="mt-1 block w-full rounded-md border-gray-300">
+                                <option value="">All</option>
+                                <option value="dog" {{ old('type') == 'dog' ? 'selected' : '' }}>Dog</option>
+                                <option value="cat" {{ old('type') == 'cat' ? 'selected' : '' }}>Cat</option>
+                                <option value="bird" {{ old('type') == 'bird' ? 'selected' : '' }}>Bird</option>
+                                <option value="fish" {{ old('type') == 'fish' ? 'selected' : '' }}>Fish</option>
+                                <option value="reptile" {{ old('type') == 'reptile' ? 'selected' : '' }}>Reptile</option>
+                                <option value="hamster" {{ old('type') == 'hamster' ? 'selected' : '' }}>Hamster</option>
+                                <option value="insect" {{ old('type') == 'insect' ? 'selected' : '' }}>Insect</option>
+                                <option value="other" {{ old('type') == 'other' ? 'selected' : '' }}>Other</option>
+                            </select>
+                            <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-2">Filter</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
 
         <div class="mt-6 bg-white shadow-sm rounded-lg divide-y">
             @each('pets.show', $pets, 'pet')
